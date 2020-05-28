5e5fac2ba4013165c1ab871c9fa69471d4cbb092
Bolubee101 <akinwandeakinboluwarin@gmail.com>
2020-05-28 09:24:02 +0100

    login and signup form included

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # To-do-list-front-end
-Clone this repo and create a backend api to satisfy the given features
+fork this repo, clone to your local machine then create a backend api to satisfy the given features
+
+1. Go to the app.js file and add the endpoint to where your server is running. Do not end with "/". It is the first line in the app.js file
+2. When the "/login" route is accessed, serve the login page.
+3. When the "/signup" route is accessed, serve the signup page.
+4. The login form data would be submitted to the "/login_submit" route via POST and a session would be created. After a session is created, the user would be redirected to the "/todo" route which serves the index.html. If no session exists and the "/todo" route is accessed, the user would be redirected to the "/login" route. If the login details are invalid, resend the login form.
+5. The signup form data would be submitted to the "/signup_submit" route via POST method and a session would be created before the user is redirected to the "/todo" route.
+6. the data submitted by the "/login_submit" route is in the form 
+{
+          "name": name,
+          "password": password
+        }
+while that of the "/signup_submit" route is 
+{
+          "name": name,
+          "password": password,
+          "email":email
+        }
+
+7. Data would be posted to the "/save" route in form of JSON data which would be an array of objects. It should be saved against the user logged in.
+8. if the "/clear" route is accessed, stored data should be deleted.
+9. If the "/data" route is accessed, the data stored against the user should be sent as it was stored.
+10. You may use any form of persistent storage you want.
+11. All complaints should be made as soon as possible. God no go shame us. this is a Three day task for Day 28 and 29 and 30.
+
+Note, you may investigate the index.html form and create a way by which the user would set their own background picture. If not though, enjoy the top of my head.
---
+++ js/app.js
@@ -0,0 +1,135 @@
+const base_url = "";
+
+const clear = document.querySelector(".clear");
+const dateElement = document.getElementById("date");
+const list = document.getElementById("list");
+const input = document.getElementById("input");
+
+const CHECK = "fa-check-circle";
+const UNCHECK = "fa-circle-thin";
+const LINE_THROUGH = "lineThrough";
+
+// Variables
+let LIST, id;
+
+//get item from local storage
+let data = Get();
+
+if (data) {
+  LIST = JSON.parse(data);
+  id = LIST.length;
+  loadList(LIST);
+} else {
+  LIST = [];
+  id = 0;
+}
+//   load list
+function loadList(array) {
+  array.forEach(function (item) {
+    addToDO(item.name, item.id, item.done, item.trash);
+  });
+}
+
+//clear the storage
+clear.addEventListener("click", function () {
+  Clear();
+  location.reload();
+});
+
+// Show todays date
+const options = { weekday: "long", month: "short", day: "numeric" };
+const today = new Date();
+
+dateElement.innerHTML = today.toLocaleDateString("en-US", options);
+
+function addToDO(toDo, id, done, trash) {
+  if (trash) {
+    return;
+  }
+
+  const DONE = done ? CHECK : UNCHECK;
+  const LINE = done ? LINE_THROUGH : "";
+  const item = `<li class="item">
+                <i class="fa ${DONE} co" job="complete" id="${id}"></i>
+                <p class="text ${LINE}" >${toDo}</p>
+  <i class="fa fa-trash-o de" job="delete" id="${id}"></i>
+</li>
+
+    `;
+  const position = "beforeend";
+  list.insertAdjacentHTML(position, item);
+}
+
+document.addEventListener("keyup", function (even) {
+  if (event.keyCode == 13) {
+    const toDo = input.value;
+    if (toDo) {
+      addToDO(toDo, id, false, false);
+
+      LIST.push({
+        name: toDo,
+        id: id,
+        done: false,
+        trash: false,
+      });
+      Save(LIST);
+      id++;
+    }
+    input.value = "";
+  }
+});
+
+//COMPLETE TO DO
+function completeToDo(element) {
+  element.classList.toggle(CHECK);
+  element.classList.toggle(UNCHECK);
+  element.parentNode.querySelector(".text").classList.toggle(LINE_THROUGH);
+
+  LIST[element.id].done = LIST[element.id].done ? false : true;
+}
+
+//REMOVE TO DO
+function removeToDo(element) {
+  element.parentNode.parentNode.removeChild(element.parentNode);
+  LIST[element.id].trash = true;
+}
+
+list.addEventListener("click", function (event) {
+  const element = event.target;
+  const elementJob = element.attributes.job.value;
+
+  if (elementJob == "complete") {
+    completeToDo(element);
+  } else if (elementJob == "delete") {
+    removeToDo(element);
+  }
+  Save(LIST);
+});
+
+function Save(list) {
+  let data = JSON.stringify(list);
+
+  let request = new XMLHttpRequest();
+  let url = base_url + "/save";
+  request.open("POST", url, true);
+  request.setRequestHeader("Content-Type", "application/json");
+  request.send(data);
+}
+
+function Get() {
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      var myObj = JSON.parse(this.response);
+      return myObj;
+    }
+  };
+  xmlhttp.open("GET", base_url + "/data", true);
+  xmlhttp.send();
+}
+
+function Clear() {
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("GET", base_url + "/clear", true);
+  xmlhttp.send();
+}
