8eff6538153cd70817f605b4187958fa9ea77cc5
Bolubee101 <akinwandeakinboluwarin@gmail.com>
2020-05-31 01:28:23 +0100

    again

--- js/login.js
+++ js/login.js
@@ -1,27 +1,30 @@
-const base_url="";
+const base_url = "";
 const contactForm = document.getElementById("submit-form");
-const loginlink=document.getElementById("form-foot");
-loginlink.insertAdjacentHTML("beforeend", `<p>dont have an account, sign up <a href="${base_url}/signup"> here</a></p>`);
+const loginlink = document.getElementById("form-foot");
+loginlink.insertAdjacentHTML(
+  "beforeend",
+  `<p>dont have an account, sign up <a href="${base_url}/signup"> here</a></p>`
+);
 contactForm.addEventListener("submit", function (event) {
-event.preventDefault();
+  event.preventDefault();
 
-let request = new XMLHttpRequest();
-let url = base_url + "/login_submit";
-request.open("POST", url, true);
-request.setRequestHeader("Content-Type", "application/json");
-request.onreadystatechange = function () {
-  if (request.readyState === 4 && request.status === 200) {
-    var jsonData = JSON.parse(request.response);
-    console.log(jsonData);
-  }
-};
-let name = document.getElementById("name").value;
-let password = document.getElementById("password").value;
+  let request = new XMLHttpRequest();
+  let url = base_url + "/login_submit";
+  request.open("POST", url, true);
+  request.setRequestHeader("Content-Type", "application/json");
+  request.onreadystatechange = function () {
+    if (request.readyState === 4 && request.status === 200) {
+      var jsonData = JSON.parse(request.response);
+      console.log(jsonData);
+    }
+  };
+  let name = document.getElementById("name").value;
+  let password = document.getElementById("password").value;
 
-let data = JSON.stringify({
-  name: name,
-  password: password
-});
+  let data = JSON.stringify({
+    name: name,
+    password: password,
+  });
 
-request.send(data);
+  request.send(data);
 });
--- js/signup.js
+++ js/signup.js
@@ -1,27 +1,31 @@
-const base_url="";
+const base_url = "";
 const contactForm = document.getElementById("submit-form");
-const loginlink=document.getElementById("form-foot");
-loginlink.insertAdjacentHTML("beforeend", `<p>already have an account?, login here: <a href="${base_url}/login"> here</a></p>`);
+const loginlink = document.getElementById("form-foot");
+loginlink.insertAdjacentHTML(
+  "beforeend",
+  `<p>already have an account?, login here: <a href="${base_url}/login"> here</a></p>`
+);
 contactForm.addEventListener("submit", function (event) {
-event.preventDefault();
+  event.preventDefault();
 
-let request = new XMLHttpRequest();
-let url = base_url + "/signup_submit";
-request.open("POST", url, true);
-request.setRequestHeader("Content-Type", "application/json");
-request.onreadystatechange = function () {
-if (request.readyState === 4 && request.status === 200) {
-var jsonData = JSON.parse(request.response);
-console.log(jsonData);
-}
-};
-let name = document.getElementById("name").value;
-let password = document.getElementById("password").value;
-let email=document.getElementById("email").value;
-let data = JSON.stringify({
-name: name,
-password: password
-});
+  let request = new XMLHttpRequest();
+  let url = base_url + "/signup_submit";
+  request.open("POST", url, true);
+  request.setRequestHeader("Content-Type", "application/json");
+  request.onreadystatechange = function () {
+    if (request.readyState === 4 && request.status === 200) {
+      var jsonData = JSON.parse(request.response);
+      console.log(jsonData);
+    }
+  };
+  let name = document.getElementById("name").value;
+  let password = document.getElementById("password").value;
+  let email = document.getElementById("email").value;
+  let data = JSON.stringify({
+    name: name,
+    password: password,
+    email:email
+  });
 
-request.send(data);
+  request.send(data);
 });
